80ee30148064bb29f8508192b7250477fc44f34d
ElieBamunoba <eliebamunoba@gmail.com>
2022-08-27 17:45:46 +0300

    core-hearders

--- property_backendss/settings.py
+++ property_backendss/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework', # django rest_framework
     'rest_framework.authtoken',
+    'corsheaders',
     
     'users',
     'authentication',
@@ -29,6 +30,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -64,6 +66,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "property_backendss.wsgi.application"
+CORS_ORIGIN_WHITELIST=[
+    "http://localhost:3000"
+]
+
 
 
 # Database
